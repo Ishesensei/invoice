046f36d61cb6ac835c4536494bfe0715f91b617d
theo <ishesensei@gmail.com>
2023-03-07 10:09:50 +0530

    d

--- taxmath.js
+++ taxmath.js
@@ -13,22 +13,81 @@ var taxable = document.querySelector(".frame71-frame292 .Editable-Text");
 var cgst = document.querySelector(".frame71-frame301 .Editable-Text");
 var sgst = document.querySelector(".frame71-frame311 .Editable-Text");
 
-
-var totalTaxableAmt = (
-  Number((document.querySelector(".frame71-frame292 .Editable-Text").innerHTML.match(/[0-9]/g)||"").toString().replace(/[^0-9]/g,""))+
-  Number((document.querySelector(".frame71-frame294 .Editable-Text").innerHTML.match(/[0-9]/g)||"").toString().replace(/[^0-9]/g,""))+
-  Number((document.querySelector(".frame71-frame296 .Editable-Text").innerHTML.match(/[0-9]/g)||"").toString().replace(/[^0-9]/g,""))+
-  Number((document.querySelector(".frame71-frame298 .Editable-Text").innerHTML.match(/[0-9]/g)||"").toString().replace(/[^0-9]/g,""))
+var totalTaxableAmt =
+  Number(
+    (
+      document
+        .querySelector(".frame71-frame292 .Editable-Text")
+        .innerHTML.match(/[0-9]/g) || ""
+    )
+      .toString()
+      .replace(/[^0-9]/g, "")
+  ) +
+  Number(
+    (
+      document
+        .querySelector(".frame71-frame294 .Editable-Text")
+        .innerHTML.match(/[0-9]/g) || ""
+    )
+      .toString()
+      .replace(/[^0-9]/g, "")
+  ) +
+  Number(
+    (
+      document
+        .querySelector(".frame71-frame296 .Editable-Text")
+        .innerHTML.match(/[0-9]/g) || ""
+    )
+      .toString()
+      .replace(/[^0-9]/g, "")
+  ) +
+  Number(
+    (
+      document
+        .querySelector(".frame71-frame298 .Editable-Text")
+        .innerHTML.match(/[0-9]/g) || ""
+    )
+      .toString()
+      .replace(/[^0-9]/g, "")
   );
 
-var totalTax = (
-  Number((document.querySelector(".frame71-frame292 .Editable-Text").innerHTML.match(/[0-9]/g)||"").toString().replace(/[^0-9]/g,""))+
-  Number((document.querySelector(".frame71-frame294 .Editable-Text").innerHTML.match(/[0-9]/g)||"").toString().replace(/[^0-9]/g,""))+
-  Number((document.querySelector(".frame71-frame296 .Editable-Text").innerHTML.match(/[0-9]/g)||"").toString().replace(/[^0-9]/g,""))+
-  Number((document.querySelector(".frame71-frame298 .Editable-Text").innerHTML.match(/[0-9]/g)||"").toString().replace(/[^0-9]/g,""))
-
-
-)
+var totalTax =
+  Number(
+    (
+      document
+        .querySelector(".frame71-frame301 .Editable-Text")
+        .innerHTML.match(/[0-9]/g) || ""
+    )
+      .toString()
+      .replace(/[^0-9]/g, "")
+  ) +
+  Number(
+    (
+      document
+        .querySelector(".frame71-frame302 .Editable-Text")
+        .innerHTML.match(/[0-9]/g) || ""
+    )
+      .toString()
+      .replace(/[^0-9]/g, "")
+  ) +
+  Number(
+    (
+      document
+        .querySelector(".frame71-frame303 .Editable-Text")
+        .innerHTML.match(/[0-9]/g) || ""
+    )
+      .toString()
+      .replace(/[^0-9]/g, "")
+  ) +
+  Number(
+    (
+      document
+        .querySelector(".frame71-frame304 .Editable-Text")
+        .innerHTML.match(/[0-9]/g) || ""
+    )
+      .toString()
+      .replace(/[^0-9]/g, "")
+  );
 var netPayable = document.querySelector(".frame71-frame532 .Editable-Text");
 
 function calcTax() {
@@ -36,22 +95,26 @@ function calcTax() {
   // code
   var taxableamt = nights.innerHTML * perNight.innerHTML;
   taxable.innerHTML = taxableamt;
-  
-  function cgstcalculator(taxableamt) {
+
+  function cgstcalculator(taxableamt, totalTaxableAmt) {
     // cgst calc
     if (taxableamt <= 7500) {
       console.log(" amt below 7500 calccgst & sgst executed");
-     var cgstcalc = Math.round((taxableamt * 12) / 100 / 2);
+      var cgstcalc = Math.round((taxableamt * 12) / 100 / 2);
     } else if (taxableamt < 7500) {
       console.log("amt below 7500 calccgst & sgst executed");
       var cgstcalc = Math.round((taxableamt * 18) / 100 / 2);
     }
+    // replace taxable amts
+    document.querySelector(".frame71-frame53 .Editable-Text").innerHTML =
+      totalTaxableAmt;
+    // document.querySelector(".frame71-frame53 .Editable-Text").innerHTML =
+    //   totalTaxAmtsssss;
+
     console.log(` returned ${cgstcalc}`);
     return cgstcalc;
   }
 
-  cgst.innerHTML = cgstcalculator(taxableamt);;
-  sgst.innerHTML = cgstcalculator(taxableamt);;
+  cgst.innerHTML = cgstcalculator(taxableamt);
+  sgst.innerHTML = cgstcalculator(taxableamt);
 }
-
-
