ace3e15e07c6226556708a01863eb0f5000e9a07
theo <ishesensei@gmail.com>
2023-03-07 08:01:17 +0530

    cgst sgst working

--- taxmath.js
+++ taxmath.js
@@ -1,3 +1,5 @@
+console.log("tax math executed");
+
 var nights = document.querySelector(".frame71-frame272 .Editable-Text");
 var perNight = document.querySelector(".frame71-frame291 .Editable-Text");
 
@@ -7,7 +9,7 @@ perNight.tabIndex = -1;
 var taxable = document.querySelector(".frame71-frame292 .Editable-Text");
 
 var cgst = document.querySelector(".frame71-frame301 .Editable-Text");
-var sgst = document.querySelector(".frame71-frame311 Editable-Text");
+var sgst = document.querySelector(".frame71-frame311 .Editable-Text");
 
 var totalTaxableAmount = document.querySelector(
   ".frame71-frame53 .Editable-Text"
@@ -16,7 +18,24 @@ var totalTax = document.querySelector(".frame71-frame531 .Editable-Text");
 var netPayable = document.querySelector(".frame71-frame532 .Editable-Text");
 
 function calcTax() {
-  console.log("calctax executed");
+  console.log("calctaxable executed");
   // code
- taxable.innerHTML = nights.innerHTML*perNight.innerHTML;
+  var taxableamt = nights.innerHTML * perNight.innerHTML;
+  taxable.innerHTML = taxableamt;
+  
+  function cgstcalculator(taxableamt) {
+    // cgst calc
+    if (taxableamt <= 7500) {
+      console.log(" amt below 7500 calccgst & sgst executed");
+     var cgstcalc = Math.round((taxableamt * 12) / 100 / 2);
+    } else if (taxableamt < 7500) {
+      console.log("amt below 7500 calccgst & sgst executed");
+      var cgstcalc = Math.round((taxableamt * 18) / 100 / 2);
+    }
+    console.log(` returned ${cgstcalc}`);
+    return cgstcalc;
+  }
+
+  cgst.innerHTML = cgstcalculator(taxableamt);;
+  sgst.innerHTML = cgstcalculator(taxableamt);;
 }
